34429b86dc7be491af0a745903f14d5323ea6350
Romain Fanara <romain.fanara@gmail.com>
2015-03-20 02:31:21 +0100

    add variable

--- application/php/configDB.php
+++ application/php/configDB.php
@@ -6,4 +6,6 @@ define('DB_USERNAME', 'user name of MySQL');
 define('DB_PASSWORD', 'password of MySQL');
 define('DB_HOST', 'host of MySQL Data Base');
 define('DB_NAME', 'name of DataBase');
+
+define('MAIL_FROM', 'email_address@example.com');
 ?>
